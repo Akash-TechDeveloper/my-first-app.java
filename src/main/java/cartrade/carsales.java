487403a25a60c578ce7889d8c8693a5eac97f73f
Akash-appdeveoper <akashdharun2020@gmail.com>
2024-06-20 16:11:12 +0530

    service provied

---
+++ src/main/java/cartrade/carsales.java
@@ -0,0 +1,20 @@
+package cartrade;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class carsales {
+
+	public static void main(String[] args) {
+		String[] car = {"Volvo", "BMW", "Ford", "Mazda"};
+	    car[0] = "Opel";
+	    for(String i :car) {
+	    	System.out.println(i);
+	    	break;
+	    }
+	    System.out.println("enter the car no");
+		
+		
+	}
+
+}
