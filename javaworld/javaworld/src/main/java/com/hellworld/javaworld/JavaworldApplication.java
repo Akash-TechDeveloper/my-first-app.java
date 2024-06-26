7d12d4adeb791f6f838d44400708f2dc6df7730b
Akash-appdeveoper <akashdharun2020@gmail.com>
2024-06-26 18:54:54 +0530

    new API project

---
+++ javaworld/javaworld/src/main/java/com/hellworld/javaworld/JavaworldApplication.java
@@ -0,0 +1,13 @@
+package com.hellworld.javaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaworldApplication.class, args);
+	}
+
+}
---
+++ javaworld/javaworld/src/test/java/com/hellworld/javaworld/JavaworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellworld.javaworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
